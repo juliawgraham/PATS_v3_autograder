0e1bf7219b19890504d6588ad06a2e3fcea5755e
Julia Graham <juliagra@andrew.cmu.edu>
2024-04-04 14:33:58 -0400

    initial controllers and tests

---
+++ app/controllers/owners_controller.rb
@@ -0,0 +1,62 @@
+class OwnersController < ApplicationController
+    # A callback to set up an @owner object to work with 
+    before_action :set_owner, only: [:show, :edit, :update, :destroy]
+    before_action :check_login
+    authorize_resource
+  
+    def index
+      # finding all the active owners and paginating that list (will_paginate)
+      @active_owners = Owner.active.alphabetical.paginate(page: params[:page]).per_page(15)
+    end
+  
+    def show
+      # authorize! :show, @owner
+      # get all the pets for this owner
+      @current_pets = @owner.pets.alphabetical.active.to_a
+    end
+  
+    def new
+      @owner = Owner.new
+    end
+  
+    def edit
+    end
+  
+    def create
+      @owner = Owner.new(owner_params)
+      if @owner.save
+        flash[:notice] = "Successfully created #{@owner.proper_name}."
+        redirect_to owner_path(@owner) 
+      else
+        render action: 'new'
+      end      
+    end
+  
+    def update
+      respond_to do |format|
+        if @owner.update(owner_params)
+          format.html { redirect_to(@owner, :notice => "Successfully updated #{@owner.proper_name}.") }
+          format.json { respond_with_bip(@owner) }
+        else
+          format.html { render :action => "edit" }
+          format.json { respond_with_bip(@owner) }
+        end
+      end
+    end
+  
+    def destroy
+      ## We don't allow destroy (will deactivate instead)
+      @owner.deactive_owner_and_pets
+      redirect_to owners_url, notice: "Successfully deactivated #{@owner.proper_name} along with associated pets."
+    end
+  
+    private
+      def set_owner
+        @owner = Owner.find(params[:id])
+      end
+  
+      def owner_params
+        params.require(:owner).permit(:first_name, :last_name, :street, :city, :state, :zip, :phone, :email, :active, :role, :username, :password, :password_confirmation)
+      end
+  
+  end
